f61a2544574377718973cc2c5fc4403e4065fc3f
Pippo Raimondi <filipporai@gmail.com>
2021-11-03 11:02:29 +0000

    Add readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # rekt-prevent
+
+## Start the app
+
+Install dependencies
+
+`npm install`
+
+Copy and populate the .env file
+
+`cp .sample.env .env`
+
+Run the service
+
+`npm run start`
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const getTokenPrice = require('./routes/get-token-price.js')
+const token = require('./routes/token.js')
 const portfolio = require('./routes/portfolio.js')
 
 
-app.get('/allTokens', getTokenPrice.getAllTokens)
+app.get('/allTokens', token.getAllTokens)
 app.get('/portfolio', portfolio.getPortfolio)
 app.get('/portfoliotokens', portfolio.getPortfolioTokens)
 
