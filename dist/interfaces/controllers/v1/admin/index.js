e3e02c137d5d626679344350f4bcd5b80f3d60a9
Pippo Raimondi <filipporai@gmail.com>
2021-11-06 11:38:53 +0000

    Ignore built app

--- dist/interfaces/controllers/v1/admin/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserController = exports.AuthController = void 0;
-var AuthController_1 = require("./AuthController");
-Object.defineProperty(exports, "AuthController", { enumerable: true, get: function () { return AuthController_1.AuthController; } });
-var UserController_1 = require("./UserController");
-Object.defineProperty(exports, "UserController", { enumerable: true, get: function () { return UserController_1.UserController; } });
-//# sourceMappingURL=index.js.map
--- dist/interfaces/routes/v1/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.adminRouter = exports.apiRouter = void 0;
-var api_1 = require("./api");
-Object.defineProperty(exports, "apiRouter", { enumerable: true, get: function () { return api_1.router; } });
-var admin_1 = require("./admin");
-Object.defineProperty(exports, "adminRouter", { enumerable: true, get: function () { return admin_1.router; } });
-//# sourceMappingURL=index.js.map
